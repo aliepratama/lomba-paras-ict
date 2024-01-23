8d41f18a849742ad1c3f7ca9fb113922d2a466c7
Alie Pratama <102345023+aliepratama@users.noreply.github.com>
2024-01-23 18:28:10 +0700

    feat: slicing navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./project/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Quicksand', ...defaultTheme.fontFamily.sans],
+        'serif': ['"Playfair Display"', ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        'primary': '#DE1B51',
+        'primaryhover': '#B3003E',
+        'secondary': '#2B3378',
+        'fblack': '#333333',
+        'lblack': '#424242',
+        'fwhite': '#F6F8FC',
+        'lwhite': '#D9D9D9',
+      }
+    },
   },
   plugins: [],
 }
